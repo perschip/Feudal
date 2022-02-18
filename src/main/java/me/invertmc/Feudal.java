ba5be71c0a135eda3928b968c62ba2fa704d8332
perschip <paul@perschilli.com>
2022-02-18 18:10:50 -0500

    Added Logger Messages and Config Creation

--- src/main/java/me/invertmc/Feudal.java
+++ src/main/java/me/invertmc/Feudal.java
@@ -1,43 +1,60 @@
 package me.invertmc;
 
+import java.io.File;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.invertmc.command.CommandManager;
+import me.invertmc.files.Messages;
 import net.md_5.bungee.api.ChatColor;
 
 public class Feudal extends JavaPlugin {
 	
-	
-	public static  Feudal instance;
-	
-	
-	@Override
-	public void onEnable() {
-		// Initialize Instance
-		instance = this;
-		
-		// onEnable Message
-		getLogger().info(ChatColor.GREEN + "==================");
-		getLogger().info(ChatColor.GREEN + "Feudal has been enabled!");
-		getLogger().info(ChatColor.GREEN + "==================");
-	}
-	
-	
-	@Override
-	public void onDisable() {
-		
-		// onDisable Message
-			getLogger().info(ChatColor.RED + "==================");
-			getLogger().info(ChatColor.RED + "Feudal has been disabled!");
-			getLogger().info(ChatColor.RED + "==================");
-	}
-	
-	
-	public void registerCommands() {
-		
-	}
-	
-	public static Feudal getInstance() {
-		return instance;
-	}
+	  public static Feudal instance;
+	   public Messages messagescfg;
+	  PluginDescriptionFile pdf = this.getDescription();
 
-}
+	   public void onEnable() {
+	      this.instance = this;
+	      this.getLogger().info("=======================");
+	      this.getLogger().info("Feudal Has Been Enabled!");
+	      this.getLogger().info("Verison: " + this.pdf.getVersion());
+	      this.createConfig();
+	      this.loadConfigManager();
+	      this.getLogger().info("=======================");
+	      this.getCommand("feudal").setExecutor(new CommandManager());
+	   }
+
+	   public void onDisable() {
+	   }
+
+	   private void createConfig() {
+	      try {
+	         if (!this.getDataFolder().exists()) {
+	            this.getDataFolder().mkdirs();
+	         }
+
+	         File file = new File(this.getDataFolder(), "config.yml");
+	         if (!file.exists()) {
+	            this.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "[Feudal]: &bConfig&8: &aGenerating config."));
+	            this.getConfig().options().copyDefaults(true);
+	            this.saveDefaultConfig();
+	         } else {
+	            this.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "[Feudal]: &bConfig&8: &aLoading config."));
+	         }
+	      } catch (Exception var2) {
+	         var2.printStackTrace();
+	      }
+
+	   }
+
+	   public void loadConfigManager() {
+	      this.messagescfg = new Messages();
+	      this.messagescfg.setup();
+	   }
+
+	   public static Feudal getInstance() {
+	      return instance;
+	   }
+	}
